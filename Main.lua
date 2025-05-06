1e13c3b7843ca92f5c33f4bc08850cc8e5b22ff9
João Pedro Corrêa Santos <108030089+scpjoaoo@users.noreply.github.com>
2025-05-06 09:49:57 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -10,9 +10,10 @@ Estrutura de pastas:
 
 -- Main.lua
 -- Arquivo principal (ponto de entrada)
-local Gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/scpjoaoo/db/refs/heads/main/GuiModule.lua"))()
-local ParryLogic = loadstring(game:HttpGet("https://raw.githubusercontent.com/scpjoaoo/db/refs/heads/main/ParryLogic.lua"))()
+local GuiModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/scpjoaoo/db/main/GuiModule.lua"))()
+local ParryLogic = loadstring(game:HttpGet("https://raw.githubusercontent.com/scpjoaoo/db/main/ParryLogic.lua"))()
 GuiModule:Init(ParryLogic)
+
 
 -- O script se manterá vivo pois RunService.Heartbeat está vinculado na lógica de parry
 -- O Gui controla a visibilidade, unload, e configuração da transparência/cor do campo de detecção
